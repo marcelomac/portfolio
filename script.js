08fd6cc026fdd122fc35eac1be1ae7f1b3bfdcf8
Marcelo A. Macagnan <marcelo.a.mac@gmail.com>
2022-07-15 13:42:27 -0300

    20220715

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-var hamburguer = document.querySelector('.hamburguer');
+var hamburguer = document.querySelector(".hamburguer");
 
-hamburguer.addEventListener('click', () => {
-  document.querySelector('.container').classList.toggle('show-menu');
-})
+hamburguer.addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("show-menu");
+});
+
+const vlrPag = 100;
+const pctJS = 1.1;
+const vlrLay = 500;
+const pctSem = 10;
+
+document.querySelector("#qtde").addEventListener("change", atualizaPreco);
+document.querySelector("#js").addEventListener("change", atualizaPreco);
+document.querySelector("#group-layout").addEventListener("change", atualizaPreco);
+document.querySelector("#prazo").addEventListener("change", () => {
+  let prazo = document.querySelector("#prazo").value;
+  document.querySelector('label[for=prazo]').innerHTML = `Prazo: ${prazo} semana(s)`;
+  atualizaPreco();
+});
+
+function atualizaPreco() {
+  const qtde = document.querySelector("#qtde").value;
+  const temJS = document.querySelector("#js").checked;
+  const laySim = document.querySelector("#layout-sim").checked;
+  const prazo = document.querySelector("#prazo").value;
+
+  let preco = qtde * vlrPag;
+  if (temJS) preco *= pctJS;
+  if (laySim) preco += vlrLay;
+
+  // preco += preco * ((prazo * pctSem)/100);
+  let taxaUrgencia = 1 - prazo * 0.1;
+
+
+  document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+}
